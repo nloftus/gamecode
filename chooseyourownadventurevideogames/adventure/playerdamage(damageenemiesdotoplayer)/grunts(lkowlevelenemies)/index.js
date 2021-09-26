c99add6cf952a89eddc20111dc4a97564dd2938d
nloftus <77587661+nloftus@users.noreply.github.com>
2021-09-26 18:54:21 -0500

    Update index.js

--- chooseyourownadventurevideogames/adventure/playerdamage(damageenemiesdotoplayer)/grunts(lkowlevelenemies)/index.js
+++ chooseyourownadventurevideogames/adventure/playerdamage(damageenemiesdotoplayer)/grunts(lkowlevelenemies)/index.js
@@ -1,9 +1,15 @@
-var life=100; //players health
+var health: float=100; //players health
 
-life = life - 10; //enemies damage to player
+var damage: float=15; //how much damage enemy does to player
+
+health = health - damage; //enemies damage to player
 
 
 
-
-
+  if (health<=0)//telling if players health is 0, then they die
+  {
+  
+      Die(); 
+  
+  }
 
