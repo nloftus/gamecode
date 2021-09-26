e9dd15eaf1c31947f35ae95d0d675835140890a8
nloftus <77587661+nloftus@users.noreply.github.com>
2021-09-26 14:07:59 -0500

    Add files via upload

---
+++ chooseyourownadventurevideogames/adventure/menu2/javascript/index.js
@@ -0,0 +1,37 @@
+//var=Variable- Variable is something you can change like from 5 to 0 or saying yes to no ect. on a website 
+
+
+
+var resume = false; //if playrer hasnt started a new game, button "resume" wont appear
+
+
+
+
+
+var newgame=false; //if player pressed button "new game" then button "resume" will appear
+
+
+
+if(newgame=true) {
+
+
+
+<script>
+
+
+<a href="/home/nathaniel/Documents/chooseyourownadventurevideogames/adventure/scenes/scene001">Resume</a>
+
+
+</script>
+
+} else {
+
+
+<script>
+
+
+<a href="/home/nathaniel/Documents/chooseyourownadventurevideogames/adventure/startnewgame(errorifsomeonepressesresumegamewhentheydidntstartanewgame)/html/index.html"></a>
+
+</script>
+
+}
---
+++ chooseyourownadventurevideogames/adventure/playerdamage(damageenemiesdotoplayer)/grunts(lkowlevelenemies)/index.js
@@ -0,0 +1,9 @@
+var life=100; //players health
+
+life = life - 10; //enemies damage to player
+
+
+
+
+
+
