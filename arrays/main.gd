9471815815b0eddfd63f17131337c77c30873fc9
nloftus <77587661+nloftus@users.noreply.github.com>
2024-09-01 10:25:42 -0500

    Add files via upload

---
+++ arrays/main.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var items: Array[String] = ["Potion", "Feather", "Stolen Harp"]
+	print(items[0])
