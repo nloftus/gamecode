68210ce8e12a607392cde3d92dda2ea171659958
nloftus <77587661+nloftus@users.noreply.github.com>
2024-09-01 11:36:31 -0500

    Update main.gd

--- random-numbers/main.gd
+++ random-numbers/main.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	 var roll = randf()
 	 if roll ,= 0.8:
 		print("You found a common item")
